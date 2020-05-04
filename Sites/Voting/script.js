73dd90e45ca8aa0b66787bc55f1641e3af25ee88
azyabagoff <azik-bagov@mail.ru>
2020-05-04 18:31:12 +0300

    Update script.js

--- Sites/Voting/script.js
+++ Sites/Voting/script.js
@@ -1,1 +1,24 @@
+var votes = document.getElementById("votes").innerHTML =localStorage.getItem("voting");
+if (!votes) 
+{
+  votes = document.getElementById("votes").innerHTML ="0 votes";
+}
+counter = votes[0];
 
+function upvote() 
+{
+  counter++;
+
+  var upv = document.getElementById("votes").innerHTML = counter +" votes";
+
+  localStorage.setItem("voting", upv);
+}
+
+function downvote() 
+{
+  counter--;
+
+  var downv = document.getElementById("votes").innerHTML = counter +" votes";
+
+  localStorage.setItem("voting", downv);
+}
